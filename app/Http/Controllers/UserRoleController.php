2bb791ee434fb9b2a423d5b1460939cad06aa532
JMS <jun.martin.setiady@gmail.com>
2025-04-11 21:48:07 +0700

    fix the return view from view('users.index') to view('Users/index')

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -10,19 +10,19 @@ class UserRoleController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('users.index', compact('users'));
+        return view('Users/index', compact('users'));
     }
 
     public function add()
     {
         $roles = Role::all();
-        return view('users.add', compact('roles'));
+        return view('Users/add', compact('roles'));
     }
 
     public function edit(User $user)
     {
         $roles = Role::all();
-        return view('users.edit', compact('user', 'roles'));
+        return view('Users/edit', compact('user', 'roles'));
     }
 
     public function assignRole(Request $request, User $user)
@@ -33,6 +33,6 @@ class UserRoleController extends Controller
 
         $user->syncRoles([$request->role]); // Remove old roles and assign new one
 
-        return redirect()->route('users.index')->with('success', 'Role assigned successfully.');
+        return redirect()->route('Users/index')->with('success', 'Role assigned successfully.');
     }
 }
